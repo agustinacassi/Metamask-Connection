9d978fae878fc0d55c19932119b759d7bca2d95a
Tini Cassi <89404026+agustinacassi@users.noreply.github.com>
2022-07-18 14:23:08 -0300

    miniproject code

---
+++ pages/index.js
@@ -0,0 +1,70 @@
+import { useWeb3React } from "@web3-react/core";
+import { useEffect } from "react";
+import { injected } from "../components/wallet/connectors";
+import { Button } from "@mui/material";
+
+export default function Home() {
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
+
+  async function connect() {
+    try {
+      await activate(injected);
+      localStorage.setItem("isWalletConnected", true);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate();
+      localStorage.setItem("isWalletConnected", false);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  useEffect(() => {
+    const connectWalletOnPageLoad = async () => {
+      if (localStorage?.getItem("isWalletConnected") === "true") {
+        try {
+          await activate(injected);
+          localStorage.setItem("isWalletConnected", true);
+        } catch (ex) {
+          console.log(ex);
+        }
+      }
+    };
+    connectWalletOnPageLoad();
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "500px",
+      }}
+    >
+      <div>
+        <Button variant="contained" onClick={connect}>
+          Connect to MetaMask
+        </Button>
+        <br></br>
+        {active ? (
+          <span>
+            Connected with <b>{account}</b>
+          </span>
+        ) : (
+          <span>Not connected</span>
+        )}
+        <br></br>
+        <Button variant="contained" onClick={disconnect}>
+          Disconnect
+        </Button>
+      </div>
+    </div>
+  );
+}
